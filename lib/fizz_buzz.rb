22441b0898dd1083464e676498a05ee6b81044cb
EC2 Default User <ec2-user@ip-172-31-52-31.ec2.internal>
2022-05-08 10:55:55 +0000

    fizz_buzzメソッド 作成

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,12 @@
 def fizz_buzz(number)
   # 実装してください
+  if number % 15 == 0
+    puts 'FizzBuzz'
+  elsif number % 3 == 0
+    puts 'Fizz'
+  elsif number % 5 == 0
+    puts 'Buzz'
+  else
+    puts number.to_s
+  end
 end
